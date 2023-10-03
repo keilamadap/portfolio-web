ddbcabe515500020a4a20a5f772d23cbb8a6a50d
Keila Amada <keila.amadap@hotmail.com>
2023-10-03 14:38:26 -0300

    first commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            Olá, eu sou Keila
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            eos tempora, quidem rem impedit placeat nesciunt facere laborum
+            nulla laudantium esse, quam est culpa nihil provident inventore
+            consequuntur natus harum.
+          </p>
+          <div>
+            <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Me contrate
+            </button>
+            <button className="px-6 py-3 rounded-full w-full bg-transparent sm:w-fit hover:bg-slate-800 text-white border border-white mt-3">
+              Currículo
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
